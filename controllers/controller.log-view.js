cb5ee5d4e7fcc8794e5ca6d483f1ac8fbaa06ec2
dikamjit_borah <dikamjit.borah@superprocure.com>
2022-09-09 13:16:16 +0530

    create stream for file read

--- controllers/controller.log-view.js
+++ controllers/controller.log-view.js
@@ -4,10 +4,21 @@ const basicUtils = require('../utils/basic.utils');
 const constants = require('../utils/constants');
 
 module.exports = {
-    logView: function (req, res) {
+    logView: async function (req, res) {
         try {
             const logFilePath = path.join(__dirname, '../', constants.paths.logFile)
-            const data = fs.readFileSync(logFilePath, 'utf8');
+            const readStream = fs.createReadStream(logFilePath, {
+                flag: 'a+',
+                encoding: 'UTF-8',
+                highWaterMark: 24
+            })
+            readStream.on('data', (chunk) => {
+                console.log(chunk);
+                console.log("\n");
+            })
+            /* let data
+            await readStream.pipe(data)
+            const data = fs.readFileSync(logFilePath, 'utf8'); */
             return basicUtils.generateResponse(res, 200, { 'Content-Type': 'application/json' }, constants.messages.LOG_VIEW_SUCCESS)
         } catch (err) {
             console.error(err);
